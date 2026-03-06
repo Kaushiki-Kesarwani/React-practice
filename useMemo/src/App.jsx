76f78fca8a5b3fb53def2c714646821f6a211c02
mahak <kaushikikes@gmail.com>
2026-03-06 19:06:36 +0530

    add expensive function

--- useMemo/src/App.jsx
+++ useMemo/src/App.jsx
@@ -1,12 +1,26 @@
 
+import { useMemo, useState } from 'react'
 import './App.css'
 
 function App() {
+const [number,setNumber] = useState(0);
+const [count,setCount] = useState(0);
 
 
+function slowSquare(num){
+  console.log("Calculating...");
+  for(let i = 0;i< 1000000000;i++){}
+  return num*num;
+}
+
+const square = useMemo(()=>slowSquare(number),[number]);
   return (
     <>
-   <p> useMemo hook</p>   
+  <h2>Square : {square}</h2>
+
+  <button onClick={()=>setNumber(number+1)}>Change number</button>
+
+  <button onClick={()=>setCount(count+1)}>Increase count</button>
     </>
   )
 }
